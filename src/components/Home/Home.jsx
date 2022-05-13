3608bb22e4beb0f861b37afde9278f674d5a1b9f
Kykeon <paulinoandresmiguel@gmail.com>
2022-05-13 11:45:41 -0400

    Add side menu with isOpen transition

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import page from '../../assets/HomePage.png';
 import './Home.scss';
-import Navigation from '../SideNav/Navigation';
+import SideNavigation from '../SideNavigation/SideNavigation';
 
 function Home() {
   return (
     <div className='container'>
-          <img className='page' src={page} alt="Home Page" />
-            <Navigation />
+      <SideNavigation />
+      <img className='page' src={page} alt='Home Page' />
+
+      <SideNavigation />
     </div>
   );
 }
