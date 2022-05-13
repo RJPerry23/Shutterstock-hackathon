669d721293a90478974bb68ee915ae3be63fefcc
Robert Perry <101219279+RJPerry23@users.noreply.github.com>
2022-05-13 14:52:36 -0400

    remove unused variable

--- src/components/LoggedIn/LoggedIn.jsx
+++ src/components/LoggedIn/LoggedIn.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import page from '../../assets/HomePage.png';
 import './LoggedIn.scss';
 import SideNavigation from '../SideNavigation/SideNavigation';
-import { Link } from 'react-router-dom';
 
 function LoggedIn() {
   return (
