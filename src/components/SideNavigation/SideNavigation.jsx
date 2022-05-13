8ce4856dfc730c15fcae1cd4d5868db55c81ec44
Kykeon <paulinoandresmiguel@gmail.com>
2022-05-13 12:58:41 -0400

    merge with develop

--- src/components/SideNavigation/SideNavigation.jsx
+++ src/components/SideNavigation/SideNavigation.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './SideNavigation.scss';
 import homeIcon from '../../assets/homeIcon.png';
