e5bdb4c6113b934d9cb5511eb24f17eb2fad547c
Kykeon <paulinoandresmiguel@gmail.com>
2022-05-13 12:55:10 -0400

    Add collborator transition

--- src/components/SideNavigation/SideNavigation.jsx
+++ src/components/SideNavigation/SideNavigation.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './SideNavigation.scss';
 import homeIcon from '../../assets/homeIcon.png';
@@ -7,15 +7,20 @@ import fileIcon from '../../assets/fileIcon.png';
 import GroupIcon from '../../assets/GroupIcon.png';
 import helpIcon from '../../assets/helpIcon.png';
 import paintIcon from '../../assets/paintIcon.png';
+import ContributorBar from '../ContributorBar/ContributorBar';
 
 function SideNavigation() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div>
+    <div className='container'>
       <nav className='nav nav--open'>
         <ul className='nav__list'>
-          <li className='nav__item'>
+          <li className='nav__item nav__item--active'>
             <Link to='/dashboard' className='nav__link'>
               <img className='nav__icon nav__icon--active homeIcon' src={homeIcon} alt='calendar' />
             </Link>
@@ -33,7 +38,7 @@ function SideNavigation() {
             </Link>
             <span className='nav__text'>Collection</span>
           </li>
-          <li className='nav__item'>
+          <li className='nav__item' onClick={handleOpen}>
             <Link to='/' className='nav__link'>
               <img className='nav__icon' src={paintIcon} alt='calendar' />
             </Link>
@@ -53,6 +58,9 @@ function SideNavigation() {
           </li>
         </ul>
       </nav>
+      <div className={`contributorBar ${isOpen && 'contributorBar--open'}`}>
+        <ContributorBar />
+      </div>
     </div>
   );
 }
