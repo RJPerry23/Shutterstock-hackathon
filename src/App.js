912c253eb88dac7f482e01c95466ee4833159b0f
Robert Perry <101219279+RJPerry23@users.noreply.github.com>
2022-05-13 06:42:18 -0400

    Updates

--- src/App.js
+++ src/App.js
@@ -6,35 +6,11 @@ import { Component } from 'react';
 
 class App extends Component{
 
-  state = {
-    animationClass: 'test'
-}
 
-//For background animation
-constructor (props) {
-    super(props);
-    this.state = {
-      animationClass: 'test'
-    }
-    this.animationState = this.animationState.bind(this);
-  }
-  
-  animationState(){
-    if(this.state.animationClass === 'test'){
-      this.setState({
-        animationClass: 'test paused'
-      });
-    }else{
-      this.setState({
-        animationClass: 'test'
-      });
-    }
-  } 
 
   render() {
     return (
       <div className="App">
-        <div className={this.state.animationClass}>
         <BrowserRouter>
         <Nav/>
         <Switch>
@@ -47,8 +23,9 @@ constructor (props) {
           <Route path='/' component={Dashboard}/> */}
         </Switch>
         </BrowserRouter>
+
+
         </div>
-      </div>
     );
   
   }
