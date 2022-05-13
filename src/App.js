d8871306a3448bb4391a8fe4d8668249836f0d95
Kykeon <paulinoandresmiguel@gmail.com>
2022-05-13 13:37:06 -0400

    Testing

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './styles/App.scss';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
 import { Component } from 'react';
-import Home from './components/LoggedIn/LoggedIn';
 import LoggedIn from './components/LoggedIn/LoggedIn';
+// import Home from './components/Home/Home';
 
 class App extends Component {
   render() {
@@ -11,7 +11,7 @@ class App extends Component {
       <div className='App'>
         <BrowserRouter>
           <Switch>
-            <Route exact path='/' component={Home} />
+            {/* <Route exact path='/' component={Home} /> */}
             <Route exact path='/login' component={LoggedIn} />
             <Route path='/dashboard' exact component={Dashboard} />
             <Route path='/dashboard/:page' component={Dashboard} />
